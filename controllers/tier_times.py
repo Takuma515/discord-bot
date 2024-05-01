f1a15d773fedd704b3a160fe38584c8c6af37615
Takuma515 <red-puma2410@docomo.ne.jp>
2024-05-01 12:41:46 +0900

    tier_timeコマンドで中央値を表示するよう変更

--- controllers/tier_times.py
+++ controllers/tier_times.py
@@ -28,15 +28,16 @@ def show_tier_time(
     track_records = sheet.fetch_track_records(track_id)
     _, wr_time, _ = sheet.fetch_wr_info(track_id)
 
-    embed = discord.Embed(title = f'Tier Time of {track_name}', color = color_green)
+    embed = discord.Embed(title = f'Tier Time of {track_name} (Median time)', color = color_green)
     embed.set_thumbnail(url = get_thumbnail_url(track_id))
 
     tier_name = ['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Sapphire', \
         'Ruby', 'Diamond', 'Master', 'Grandmaster']
     tier_range = [0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000, 17000, 10*5]
-    tier_time_info = [[0] * 2 for _ in range(10)]	# [cnt, sum_time]
+    tier_times = [[] for _ in range(len(tier_name))]
 
-    # tierごとにタイムを集計
+    
+    # tierごとにタイムを分けてリストに格納
     for i in range(min(len(mmr_list), len(track_records))):
         mmr = mmr_list[i]
         record = track_records[i]
@@ -48,10 +49,10 @@ def show_tier_time(
             min_mmr, max_mmr = tier_range[j], tier_range[j+1]
             
             if min_mmr <= int(mmr) < max_mmr:
-                tier_time_info[j][0] += 1
-                tier_time_info[j][1] += convert_time_into_seconds(record)
+                tier_times[j].append(record)
                 break
-    
+
+
     col_id = sheet.search_user(ctx.author)
     user_time = sheet.fetch_user_record(track_id, col_id)
 
@@ -61,17 +62,19 @@ def show_tier_time(
     else:
         diff = calc_time_diff(user_time, wr_time)
         embed.add_field(name='Your Record', value=f'> {format_time(user_time)} (WR {format_diff(diff)})', inline=False)
-    
-    for i in range(len(tier_name)):
-        cnt, sum_time = tier_time_info[i]
 
-        if cnt == 0:
-            embed.add_field(name=f'{tier_name[i]} / N = {cnt}', value='-', inline=False)
+    
+    # 中央値をembedに追加
+    for i in range(len(tier_name)):
+        times = tier_times[i]
+        if len(times) == 0:
+            embed.add_field(name=f'{tier_name[i]} / N = 0', value='-', inline=False)
             continue
 
-        avg_time = convert_seconds_into_time(sum_time / cnt)
-        diff = calc_time_diff(avg_time, wr_time)
-        embed.add_field(name=f'{tier_name[i]} / N = {cnt}', value=f'> {format_time(avg_time)} (WR {format_diff(diff)})', inline=False)
-
+        times.sort()
+        median_time = times[len(times) // 2]
+        diff = calc_time_diff(median_time, wr_time)
+        embed.add_field(name=f'{tier_name[i]} / N = {len(times)}', value=f'> {format_time(median_time)} (WR {format_diff(diff)})', inline=False)
+    
     
     return embed
